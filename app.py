c0922995833f5ab0d03cf71d291e2c8328ed46e8
Gabriel Ekodo <ekodogabriel@outlook.com>
2022-08-06 11:29:11 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,104 @@
+
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+
+from flask_sqlalchemy import SQLAlchemy
+import sys
+from flask_migrate import Migrate
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean)
+    dueDate = db.Column(db.Date, nullable=False)
+
+    def __repr__(self):
+        return f'<Description ID: {self.id}, name: {self.description}>'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
+
+
+@app.route('/tasks/create', methods=['POST'])
+def create_task():
+    error = False
+    body = {}
+    try:
+        dueDate = request.form.get_json()['due-date']
+        task = request.form.get_json()['description']
+        todo = Todo(description=task, dueDate=dueDate)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+        body['due-date']=todo.dueDate
+
+
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+
+    finally:
+        db.session.close()
+    if error:
+        abort (400)
+    else:
+        return jsonify(body)    
+
+    # finally:
+    #     db.session.close()
+    #     return redirect(url_for('index'))
+        # if not error:
+        #     return jsonify(body)
+
+
+# updating task
+@app.route('/tasks/<taskId>/set-completed', methods=['POST'])
+def set_completed(taskId):
+
+    try:
+        task_completed = request.form.get_json()['completed']
+        todo = Todo.query.get(taskId)
+        print(todo)
+        todo.completed = task_completed
+
+        db.session.commit()
+
+    except:
+
+        db.session.rollback()
+        print(sys.exc_info())
+
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+# deleting task
+@app.route('/tasks/<taskId>/delete', methods=['GET'])
+def delete_task(taskId):
+
+    try:
+
+        Todo.query.filter_by(id=taskId).delete()
+
+        db.session.commit()
+
+    except:
+
+        db.session.rollback()
+        print(sys.exc_info())
+
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
